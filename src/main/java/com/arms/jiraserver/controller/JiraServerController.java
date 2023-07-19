1dd1dd194e8f55d7959c68f95066ac6e5e249943
LeeDongMin <admin@313.co.kr>
2023-07-19 19:40:37 +0900

    feat: [ARMSQA-19] #comment jira server data mining function develop - 23.07.19 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/jiraserver/controller/JiraServerController.java
+++ src/main/java/com/arms/jiraserver/controller/JiraServerController.java
@@ -13,19 +13,25 @@ package com.arms.jiraserver.controller;
 
 import com.arms.jiraserver.model.JiraServerDTO;
 import com.arms.jiraserverlog.model.JiraServerLogEntity;
+import com.egovframework.javaservice.treeframework.controller.CommonResponse;
 import com.egovframework.javaservice.treeframework.controller.TreeAbstractController;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 
 import com.arms.jiraserver.model.JiraServerEntity;
 import com.arms.jiraserver.service.JiraServer;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -44,4 +50,12 @@ public class JiraServerController extends TreeAbstractController<JiraServer, Jir
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @ResponseBody
+    @RequestMapping(
+            value = {"/miningDataToaRMS.do"},
+            method = {RequestMethod.GET}
+    )
+    public ResponseEntity<?> miningDataToaRMS(ModelMap model, HttpServletRequest request) throws Exception {
+        return ResponseEntity.ok(CommonResponse.success(jiraServer.miningDataToaRMS()));
+    }
 }
--- src/main/java/com/arms/jiraserver/service/JiraServerImpl.java
+++ src/main/java/com/arms/jiraserver/service/JiraServerImpl.java
@@ -11,12 +11,25 @@
  */
 package com.arms.jiraserver.service;
 
+import com.arms.jiraissuetype.model.JiraIssueTypeEntity;
+import com.arms.jiraserver.model.JiraServerEntity;
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.IssueType;
+import com.atlassian.jira.rest.client.api.domain.ServerInfo;
+import com.config.JiraConfig;
+import com.egovframework.javaservice.treeframework.TreeConstant;
 import com.egovframework.javaservice.treeframework.service.TreeServiceImpl;
+import com.egovframework.javaservice.treeframework.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @AllArgsConstructor
@@ -24,6 +37,55 @@ import lombok.AllArgsConstructor;
 public class JiraServerImpl extends TreeServiceImpl implements JiraServer{
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	@Qualifier("jiraConfig")
+	private JiraConfig jiraConfig;
+
+	@Override
+	@Transactional
+	public String miningDataToaRMS() throws Exception {
+		final JiraRestClient restClient = jiraConfig.getJiraRestClient();
+		ServerInfo serverInfo = restClient.getMetadataClient().getServerInfo().get();
+
+		JiraServerEntity searchNode = new JiraServerEntity();
+		List<JiraServerEntity> list = this.getNodesWithoutRoot(searchNode);
+
+		if ( list.isEmpty() ){
+			JiraServerEntity jiraServerEntity = new JiraServerEntity();
+			jiraServerEntity.setC_jira_server_contents(serverInfo.getServerTime().toString());
+			jiraServerEntity.setC_jira_server_etc(serverInfo.getScmInfo());
+			jiraServerEntity.setC_jira_server_base_url(serverInfo.getBaseUri().toString());
+			jiraServerEntity.setC_jira_server_version(serverInfo.getVersion());
+			jiraServerEntity.setC_jira_server_build(StringUtils.toString(serverInfo.getBuildNumber()));
+			jiraServerEntity.setC_jira_server_title(serverInfo.getServerTitle());
+
+			jiraServerEntity.setRef(TreeConstant.First_Node_CID);
+			jiraServerEntity.setC_type(TreeConstant.Leaf_Node_TYPE);
+
+			this.addNode(jiraServerEntity);
+
+		} else if (list.size() > 1){
+
+			logger.info("JiraServerImpl :: miningDataToaRMS :: jira server count > 1");
+
+		} else {
+
+			JiraServerEntity jiraServerEntity = list.get(0);
+
+			jiraServerEntity.setC_jira_server_contents(serverInfo.getServerTime().toString());
+			jiraServerEntity.setC_jira_server_etc(serverInfo.getScmInfo());
+			jiraServerEntity.setC_jira_server_base_url(serverInfo.getBaseUri().toString());
+			jiraServerEntity.setC_jira_server_version(serverInfo.getVersion());
+			jiraServerEntity.setC_jira_server_build(StringUtils.toString(serverInfo.getBuildNumber()));
+			jiraServerEntity.setC_jira_server_title(serverInfo.getServerTitle());
+
+			this.updateNode(jiraServerEntity);
+
+		}
+
+		return "Jira Server Data Mining Complete";
+	}
 
 
 }
