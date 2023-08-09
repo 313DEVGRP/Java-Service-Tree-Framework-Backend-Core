2ed232a3649aee17ad3b857f5a7a0e6f156357b7
LeeDongMin <admin@313.co.kr>
2023-08-09 09:27:45 +0900

    feat: [ARMSQA-19] #comment jira server 에 이슈 우선순위, 해결책, 타입, 상태값, 프로젝트 리스트를 연결함 ( 단방향 ) 1:N - 23.08.09 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/jiraserver/model/JiraServerEntity.java
+++ src/main/java/com/arms/jiraserver/model/JiraServerEntity.java
@@ -11,18 +11,27 @@
  */
 package com.arms.jiraserver.model;
 
+import com.arms.jiraissuepriority.model.JiraIssuePriorityEntity;
+import com.arms.jiraissueresolution.model.JiraIssueResolutionEntity;
+import com.arms.jiraissuestatus.model.JiraIssueStatusEntity;
+import com.arms.jiraissuetype.model.JiraIssueTypeEntity;
+import com.arms.jiraproject.model.JiraProjectEntity;
+import com.arms.pdserviceversion.model.PdServiceVersionEntity;
 import com.egovframework.javaservice.treeframework.model.TreeBaseEntity;
 import com.egovframework.javaservice.treeframework.model.TreeSearchEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.*;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -73,6 +82,107 @@ public class JiraServerEntity extends TreeSearchEntity implements Serializable {
     @Column(name = "c_jira_server_etc")
     @Type(type="text")
     private String c_jira_server_etc;
+
+    // -- 1:N table 연계 - 단방향
+    private Set<JiraIssuePriorityEntity> jiraIssuePriorityEntities;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "GLOBAL_TREE_MAP",
+            joinColumns = @JoinColumn(name = "jiraserver_link"),
+            inverseJoinColumns = @JoinColumn(name = "jiraissuepriority_link")
+    )
+    @WhereJoinTable( clause = "jiraissuepriority_link is not null")
+    public Set<JiraIssuePriorityEntity> getJiraIssuePriorityEntities() {
+        return jiraIssuePriorityEntities;
+    }
+
+    public void setJiraIssuePriorityEntities(Set<JiraIssuePriorityEntity> jiraIssuePriorityEntities) {
+        this.jiraIssuePriorityEntities = jiraIssuePriorityEntities;
+    }
+
+    // -- 1:N table 연계 - 단방향
+    private Set<JiraIssueResolutionEntity> jiraIssueResolutionEntities;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "GLOBAL_TREE_MAP",
+            joinColumns = @JoinColumn(name = "jiraserver_link"),
+            inverseJoinColumns = @JoinColumn(name = "jiraissueresolution_link")
+    )
+    @WhereJoinTable( clause = "jiraissueresolution_link is not null")
+    public Set<JiraIssueResolutionEntity> getJiraIssueResolutionEntities() {
+        return jiraIssueResolutionEntities;
+    }
+
+    public void setJiraIssueResolutionEntities(Set<JiraIssueResolutionEntity> jiraIssueResolutionEntities) {
+        this.jiraIssueResolutionEntities = jiraIssueResolutionEntities;
+    }
+
+    // -- 1:N table 연계
+    private Set<JiraIssueStatusEntity> jiraIssueStatusEntities;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "GLOBAL_TREE_MAP",
+            joinColumns = @JoinColumn(name = "jiraserver_link"),
+            inverseJoinColumns = @JoinColumn(name = "jiraissuestatus_link")
+    )
+    @WhereJoinTable( clause = "jiraissuestatus_link is not null")
+    public Set<JiraIssueStatusEntity> getJiraIssueStatusEntities() {
+        return jiraIssueStatusEntities;
+    }
+
+    public void setJiraIssueStatusEntities(Set<JiraIssueStatusEntity> jiraIssueStatusEntities) {
+        this.jiraIssueStatusEntities = jiraIssueStatusEntities;
+    }
+
+    // -- 1:N table 연계
+    private Set<JiraIssueTypeEntity> jiraIssueTypeEntities;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "GLOBAL_TREE_MAP",
+            joinColumns = @JoinColumn(name = "jiraserver_link"),
+            inverseJoinColumns = @JoinColumn(name = "jiraissuestype_link")
+    )
+    @WhereJoinTable( clause = "jiraissuestype_link is not null")
+    public Set<JiraIssueTypeEntity> getJiraIssueTypeEntities() {
+        return jiraIssueTypeEntities;
+    }
+
+    public void setJiraIssueTypeEntities(Set<JiraIssueTypeEntity> jiraIssueTypeEntities) {
+        this.jiraIssueTypeEntities = jiraIssueTypeEntities;
+    }
+
+    // -- 1:N table 연계 - 단방향
+    private Set<JiraProjectEntity> jiraProjectEntities;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "GLOBAL_TREE_MAP",
+            joinColumns = @JoinColumn(name = "jiraserver_link"),
+            inverseJoinColumns = @JoinColumn(name = "jiraproject_link")
+    )
+    @WhereJoinTable( clause = "jiraproject_link is not null")
+    public Set<JiraProjectEntity> getJiraProjectEntities() {
+        return jiraProjectEntities;
+    }
+
+    public void setJiraProjectEntities(Set<JiraProjectEntity> jiraProjectEntities) {
+        this.jiraProjectEntities = jiraProjectEntities;
+    }
+
 
     /*
      * Extend Bean Field
