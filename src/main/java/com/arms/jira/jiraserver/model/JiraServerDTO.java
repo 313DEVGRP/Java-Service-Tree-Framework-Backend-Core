98bc5716980a8b6b2756f6948d60394cd002f372
hsYang <gudtjr4662@gmail.com>
2023-08-22 18:38:56 +0900

    feat: [ARMSQA-23] #comment 지라 서버 DTO 업데이트 - 지라이슈 관련 항목들 추가 23.08.22 #close #time 1h +review SR @HSYang-Hub
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/jira/jiraserver/model/JiraServerDTO.java
+++ src/main/java/com/arms/jira/jiraserver/model/JiraServerDTO.java
@@ -1,5 +1,9 @@
 package com.arms.jira.jiraserver.model;
 
+import com.arms.jira.jiraissuepriority.model.JiraIssuePriorityEntity;
+import com.arms.jira.jiraissueresolution.model.JiraIssueResolutionEntity;
+import com.arms.jira.jiraissuestatus.model.JiraIssueStatusEntity;
+import com.arms.jira.jiraissuetype.model.JiraIssueTypeEntity;
 import com.arms.jira.jiraproject.model.JiraProjectEntity;
 import com.egovframework.javaservice.treeframework.model.TreeBaseDTO;
 import lombok.*;
@@ -30,6 +34,14 @@ public class JiraServerDTO extends TreeBaseDTO {
     private String c_jira_server_etc;
 
     private Set<JiraProjectEntity> jiraProjectEntities;   // 추가됨
+
+    private Set<JiraIssueTypeEntity> jiraIssueTypeEntities;
+
+    private Set<JiraIssuePriorityEntity> jiraIssuePriorityEntities;
+
+    private Set<JiraIssueResolutionEntity> jiraIssueResolutionEntities;
+
+    private Set<JiraIssueStatusEntity> jiraIssueStatusEntities;
 
 
 }
