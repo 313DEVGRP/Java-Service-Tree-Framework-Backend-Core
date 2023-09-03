e22c20dafce5b37c80f78f57b04c315e37b22d0f
LeeDongMin <admin@313.co.kr>
2023-09-03 11:37:59 +0900

    feat: [ARMSQA-19] #comment :white_check_mark: 지라서버가 삭제되었을 때 방어 코드 삽입 - 23.09.01 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/util/external_communicate/엔진통신기.java
+++ src/main/java/com/arms/util/external_communicate/엔진통신기.java
@@ -49,6 +49,10 @@ public interface 엔진통신기 {
                                                                          @PathVariable("projectId") String 프로젝트_아이디);
 
     @GetMapping("/engine/jira/{connectId}/issue/loadToES/bulk/{issueKey}")
-    public int 이슈_검색엔진_벌크_저장( @PathVariable("connectId") Long 지라서버_아이디, @PathVariable("issueKey") String 이슈_키);
+    public int 이슈_검색엔진_벌크_저장(
+            @PathVariable("connectId") Long 지라서버_아이디,
+            @PathVariable("issueKey") String 이슈_키,
+            @RequestParam("pdServiceId") Long 제품서비스_아이디,
+            @RequestParam("pdServiceVersion") Long 제품서비스_버전_아이디);
 
 }
