43503fc22d54b272425e7fc5f08c65c643b96c9e
LeeDongMin <admin@313.co.kr>
2023-09-17 10:05:49 +0900

    feat: [ARMSQA-19] #comment :white_check_mark: core lib 다시 머지함 - 23.09.17 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/egovframework/javaservice/treeframework/remote/Chat.java
@@ -0,0 +1,97 @@
+/*
+ * @author Dongmin.lee
+ * @since 2023-03-13
+ * @version 23.03.13
+ * @see <pre>
+ *  Copyright (C) 2007 by 313 DEV GRP, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by 313 developer group <313@313.co.kr>, December 2010
+ * </pre>
+ */
+package com.egovframework.javaservice.treeframework.remote;
+
+import org.directwebremoting.Browser;
+import org.directwebremoting.ScriptSessions;
+import org.directwebremoting.WebContext;
+import org.directwebremoting.WebContextFactory;
+import org.directwebremoting.annotations.RemoteMethod;
+import org.directwebremoting.annotations.RemoteProxy;
+import org.directwebremoting.spring.SpringCreator;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RemoteProxy(creator = SpringCreator.class, name = "Chat")
+@Service
+public class Chat {
+
+	public Chat() {
+		Global.chat = this;
+	}
+
+	public String sendMessageByEngine(String message) {
+
+		Browser.withAllSessions(new Runnable() {
+			@Override
+			public void run() {
+				ScriptSessions.addFunctionCall("dwr_callback","engine","engine", "[Engine] " + message + " " + time(),time());
+			}
+		});
+
+		return Global.SUCCESS;
+	}
+
+	public String sendMessageByServer(String message) {
+
+		Browser.withAllSessions(new Runnable() {
+			@Override
+			public void run() {
+				ScriptSessions.addFunctionCall("dwr_callback","engine","engine", "[Server] " + message + " " + time(),time());
+			}
+		});
+
+		return Global.SUCCESS;
+	}
+
+	@RemoteMethod
+	public String sendMessage(String message) {
+
+		Object userId = WebContextFactory.get().getScriptSession().getAttribute(Global.USER_ID);
+		Object username = WebContextFactory.get().getScriptSession().getAttribute(Global.USERNAME);
+		Browser.withAllSessions(new Runnable() {
+			@Override
+			public void run() {
+				ScriptSessions.addFunctionCall("dwr_callback",userId,username, message,time());
+			}
+		});
+
+		return Global.SUCCESS;
+	}
+	@RemoteMethod
+	public String login(final String userId, final String username) {
+		Global.onlineSet.add(new User(userId,username, time()));
+		WebContext webContext = WebContextFactory.get();
+		webContext.getScriptSession().setAttribute(Global.USER_ID, userId);
+		webContext.getScriptSession().setAttribute(Global.USERNAME, username);
+		return Global.SUCCESS;
+	}
+
+	@RemoteMethod
+	public String logout(final String userId) {
+		if (!Global.onlineSet.contains(new User(userId))) {
+			return Global.ERROR;
+		} else {
+			Global.onlineSet.remove(new User(userId));
+			WebContextFactory.get().getScriptSession().invalidate();
+			return Global.SUCCESS;
+		}
+	}
+
+	@RemoteMethod
+	private String time() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	}
+
+}
