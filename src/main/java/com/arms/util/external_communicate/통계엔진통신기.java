f1e2666a8500b76e9a1b141d1e7bb4317fdf54f9
dumbbelloper <dumbbelloper@gmail.com>
2023-11-23 01:27:23 +0900

    refactor: [ARMS-207] #comment SonarLint 피드백 반영 - 23.11.23
    #close #time 10m +review SR @dumbbelloper
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/util/external_communicate/통계엔진통신기.java
+++ src/main/java/com/arms/util/external_communicate/통계엔진통신기.java
@@ -19,60 +19,57 @@ import java.util.Map;
 @FeignClient(name = "engine-dashboard", url = "${arms.engine.url}")
 public interface 통계엔진통신기 {
     @GetMapping("/engine/jira/dashboard/jira-issue-statuses")
-    public ResponseEntity<검색결과_목록_메인> 제품_혹은_제품버전들의_지라이슈상태_집계(
+    ResponseEntity<검색결과_목록_메인> 제품_혹은_제품버전들의_지라이슈상태_집계(
             @SpringQueryMap 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
     );
 
     @GetMapping("/engine/jira/dashboard/requirements-jira-issue-statuses")
-    public Map<String, RequirementJiraIssueAggregationResponse> 제품_혹은_제품버전들의_요구사항_지라이슈상태_월별_집계(
-            @RequestParam Long pdServiceLink,
-            @RequestParam List<Long> pdServiceVersionLinks
+    ResponseEntity<Map<String, RequirementJiraIssueAggregationResponse>> 제품_혹은_제품버전들의_요구사항_지라이슈상태_월별_집계(
+            @SpringQueryMap 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
     );
 
     @GetMapping("/engine/jira/dashboard/issue-assignee/{pdServiceId}")
-    public Map<String, Long> 제품서비스별_담당자_이름_통계(@PathVariable("pdServiceId") Long 제품서비스_아이디);
+    Map<String, Long> 제품서비스별_담당자_이름_통계(@PathVariable("pdServiceId") Long 제품서비스_아이디);
 
     @GetMapping("/engine/jira/dashboard/version-assignees")
-    public List<검색결과> 제품_혹은_제품버전들의_담당자목록(
+    ResponseEntity<List<검색결과>> 제품_혹은_제품버전들의_담당자목록(
             @SpringQueryMap 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
     );
 
     @GetMapping("/engine/jira/dashboard/assignee-jira-issue-statuses")
-    public Map<String, Map<String, Map<String, Integer>>> 담당자_요구사항여부_상태별집계(
+    Map<String, Map<String, Map<String, Integer>>> 담당자_요구사항여부_상태별집계(
             @RequestParam Long pdServiceLink) throws IOException;
 
     @GetMapping("/engine/jira/dashboard/normal/{pdServiceId}")
-    public ResponseEntity<검색결과_목록_메인> 제품서비스_일반_통계(
+    ResponseEntity<검색결과_목록_메인> 제품서비스_일반_통계(
             @PathVariable("pdServiceId") Long 제품서비스_아이디 ,
             @SpringQueryMap 지라이슈_일반_검색_요청 지라이슈_검색_일반_요청
     );
     
     @GetMapping("/engine/jira/dashboard/isreq-normal/{pdServiceId}")
-    public ResponseEntity<검색결과_목록_메인> 제품서비스_일반_통계_개선(
+    ResponseEntity<검색결과_목록_메인> 제품서비스_일반_통계_개선(
             @PathVariable("pdServiceId") Long 제품서비스_아이디 ,
             @SpringQueryMap 지라이슈_일반_검색_요청 지라이슈_검색_일반_요청
     );
 
     @GetMapping("/engine/jira/dashboard/exclusion-isreq-normal/{pdServiceId}")
-    public ResponseEntity<Map<String, Object>> 제품서비스_요구사항제회_일반_통계(
+    ResponseEntity<Map<String, Object>> 제품서비스_요구사항제회_일반_통계(
             @PathVariable("pdServiceId") Long 제품서비스_아이디 ,
             @SpringQueryMap 지라이슈_일반_검색_요청 지라이슈_검색_일반_요청);
 
     @GetMapping("/engine/jira/dashboard/assignees-requirements-involvements")
-    public List<Worker> 작업자별_요구사항_관여도(
-            @RequestParam Long pdServiceLink,
-            @RequestParam List<Long> pdServiceVersionLinks,
-            @RequestParam int maxResults
+    ResponseEntity<List<Worker>> 작업자별_요구사항_관여도(
+            @SpringQueryMap 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
     ) throws IOException;
 
     @GetMapping("/engine/jira/dashboard/normal-version/{pdServiceId}")
-    public ResponseEntity<검색결과_목록_메인> 제품서비스_일반_버전_통계(
+    ResponseEntity<검색결과_목록_메인> 제품서비스_일반_버전_통계(
             @PathVariable("pdServiceId") Long 제품서비스_아이디 ,
             @RequestParam List<Long> pdServiceVersionLinks,
             @SpringQueryMap 지라이슈_일반_검색_요청 지라이슈_검색_일반_요청);
 
     @GetMapping("/engine/jira/dashboard/normal-version-only/{pdServiceId}")
-    public ResponseEntity<검색결과_목록_메인> 일반_버전필터_작업자별_검색(
+    ResponseEntity<검색결과_목록_메인> 일반_버전필터_작업자별_검색(
             @PathVariable("pdServiceId") Long 제품서비스_아이디 ,
             @RequestParam List<Long> pdServiceVersionLinks,
             @SpringQueryMap 지라이슈_단순_검색_요청 지라이슈_단순_검색_요청);
