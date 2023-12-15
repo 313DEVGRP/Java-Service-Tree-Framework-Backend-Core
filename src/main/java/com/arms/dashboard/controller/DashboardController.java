6a75eab34964239aac7a9b8c2843da9090071cfb
dumbbelloper <dumbbelloper@gmail.com>
2023-12-15 23:33:37 +0900

    feat: [ARMS-251] #comment 코드 정리 - 23.12.15
    - requirements-jira-issue-statuses
    - version-assignees
    - assignees-requirements-involvements
    
    #close #time 1h +review SR @dumbbelloper
    
    See http://www.313.co.kr/jira/browse/ARMS-251 more details.
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/dashboard/controller/DashboardController.java
+++ src/main/java/com/arms/dashboard/controller/DashboardController.java
@@ -2,12 +2,7 @@ package com.arms.dashboard.controller;
 
 import com.arms.dashboard.model.combination.RequirementJiraIssueAggregationResponse;
 import com.arms.dashboard.model.sankey.SankeyData;
-import com.arms.dashboard.model.sankey.SankeyLink;
-import com.arms.dashboard.model.sankey.SankeyNode;
 import com.arms.dashboard.service.DashboardService;
-import com.arms.product_service.pdservice.model.PdServiceEntity;
-import com.arms.product_service.pdservice.service.PdService;
-import com.arms.product_service.pdserviceversion.model.PdServiceVersionEntity;
 import com.arms.util.external_communicate.dto.search.검색결과;
 import com.arms.util.external_communicate.dto.search.검색결과_목록_메인;
 import com.arms.util.external_communicate.dto.지라이슈_일반_검색_요청;
@@ -22,7 +17,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import com.arms.util.external_communicate.*;
 
@@ -36,7 +30,6 @@ public class DashboardController {
 
     private final 엔진통신기 엔진통신기;
     private final 통계엔진통신기 통계엔진통신기;
-    private final PdService pdService;
     private final DashboardService dashboardService;
 
     @GetMapping("/aggregation/nested")
@@ -47,6 +40,32 @@ public class DashboardController {
     @GetMapping("/aggregation/flat")
     public ResponseEntity<ApiResult<검색결과_목록_메인>> commonFlatAggregation(지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) {
         return ResponseEntity.ok(CommonResponse.success(dashboardService.commonFlatAggregation(지라이슈_제품_및_제품버전_검색요청)));
+    }
+
+    /**
+     * C3 Donut Chart API - Dashboard
+     */
+    @GetMapping("/requirements-jira-issue-statuses")
+    public ResponseEntity<ApiResult<Map<String, RequirementJiraIssueAggregationResponse>>> requirementsJiraIssueStatuses(지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.requirementsJiraIssueStatuses(지라이슈_제품_및_제품버전_검색요청)));
+    }
+
+    /**
+     * D3 Sankey Chart API - Dashboard, Analysis Resource
+     */
+    @GetMapping("/version-assignees")
+    public ResponseEntity<ApiResult<SankeyData>> assigneesByPdServiceVersion(지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) throws Exception {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.sankeyChartAPI(지라이슈_제품_및_제품버전_검색요청)));
+    }
+
+    /**
+     * Apache Echarts TreeMap Chart API - Dashboard, Analysis Resource
+     */
+    @GetMapping("/assignees-requirements-involvements")
+    ResponseEntity<ApiResult<List<Map<String, Object>>>> 작업자별_요구사항_관여도(
+            지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
+    ) throws Exception {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.작업자별_요구사항_관여도(지라이슈_제품_및_제품버전_검색요청)));
     }
 
     @GetMapping(value = "/getVersionProgress")
@@ -82,70 +101,6 @@ public class DashboardController {
     }
 
 
-    @GetMapping("/requirements-jira-issue-statuses")
-    public ModelAndView requirementsJiraIssueStatuses(지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) {
-        Map<String, RequirementJiraIssueAggregationResponse> result = 통계엔진통신기.제품_혹은_제품버전들의_요구사항_지라이슈상태_월별_집계(지라이슈_제품_및_제품버전_검색요청).getBody();
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        modelAndView.addObject("result", result);
-        return modelAndView;
-    }
-
-    @GetMapping("/version-assignees")
-    public ModelAndView assigneesByPdServiceVersion(
-            지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
-    ) throws Exception {
-        Long pdServiceLink = 지라이슈_제품_및_제품버전_검색요청.getPdServiceLink();
-        List<Long> pdServiceVersionLinks = 지라이슈_제품_및_제품버전_검색요청.getPdServiceVersionLinks();
-        if (pdServiceVersionLinks.isEmpty()) {
-            return new ModelAndView("jsonView")
-                    .addObject("result", new SankeyData(Collections.emptyList(), Collections.emptyList()));
-        }
-
-        PdServiceEntity pdServiceEntity = new PdServiceEntity();
-        pdServiceEntity.setC_id(pdServiceLink);
-        PdServiceEntity savedPdService = pdService.getNode(pdServiceEntity);
-
-        List<SankeyNode> nodeList = new ArrayList<>();
-        List<SankeyLink> linkList = new ArrayList<>();
-
-        String pdServiceId = savedPdService.getC_id() + "-product";
-        nodeList.add(new SankeyNode(pdServiceId, savedPdService.getC_title(), "제품"));
-        nodeList.add(new SankeyNode("No-Worker", "No-Worker", "No-Worker"));
-
-        Set<Long> versionIds = savedPdService.getPdServiceVersionEntities().stream()
-                .filter(version -> pdServiceVersionLinks.contains(version.getC_id()))
-                .sorted(Comparator.comparing(PdServiceVersionEntity::getC_id))
-                .map(version -> {
-                    String versionId = version.getC_id() + "-version";
-                    nodeList.add(new SankeyNode(versionId, version.getC_title(), "버전"));
-                    linkList.add(new SankeyLink(pdServiceId, versionId));
-                    return version.getC_id();
-                })
-                .collect(Collectors.toSet());
-
-        Optional<List<검색결과>> optionalEsData = Optional.ofNullable(통계엔진통신기.제품_혹은_제품버전들의_담당자목록(지라이슈_제품_및_제품버전_검색요청).getBody());
-        optionalEsData.ifPresent(esData -> {
-            esData.forEach(result -> {
-                String versionId = result.get필드명();
-                result.get하위검색결과().get("assignees").forEach(assignee -> {
-                    String assigneeAccountId = assignee.get필드명();
-                    assignee.get하위검색결과().get("displayNames").stream().forEach(displayName -> {
-                        String assigneeDisplayName = displayName.get필드명();
-                        String workerNodeId = versionId + "-" + assigneeAccountId;
-                        nodeList.add(new SankeyNode(workerNodeId, assigneeDisplayName, "작업자"));
-                        linkList.add(new SankeyLink(versionId + "-version", workerNodeId));
-                        versionIds.remove(Long.parseLong(versionId));
-                    });
-                });
-            });
-        });
-
-        versionIds.forEach(versionId -> linkList.add(new SankeyLink(versionId + "-version", "No-Worker")));
-
-        return new ModelAndView("jsonView")
-                .addObject("result", new SankeyData(nodeList, linkList));
-    }
-
     @GetMapping("/normal/{pdServiceId}")
     public ModelAndView normal_aggs(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
         ResponseEntity<검색결과_목록_메인> 요구사항_연결이슈_일반_통계
@@ -154,16 +109,6 @@ public class DashboardController {
         ModelAndView modelAndView = new ModelAndView("jsonView");
         검색결과_목록_메인 통신결과 = 요구사항_연결이슈_일반_통계.getBody();
         modelAndView.addObject("result", 통신결과);
-        return modelAndView;
-    }
-
-    @GetMapping("/assignees-requirements-involvements")
-    public ModelAndView 작업자별_요구사항_관여도(
-            지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
-    ) throws Exception {
-        List<Map<String, Object>> result = 통계엔진통신기.작업자별_요구사항_관여도(지라이슈_제품_및_제품버전_검색요청).getBody();
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        modelAndView.addObject("result", result);
         return modelAndView;
     }
 
--- src/main/java/com/arms/dashboard/service/DashboardServiceImpl.java
+++ src/main/java/com/arms/dashboard/service/DashboardServiceImpl.java
@@ -1,5 +1,13 @@
 package com.arms.dashboard.service;
 
+import com.arms.dashboard.model.combination.RequirementJiraIssueAggregationResponse;
+import com.arms.dashboard.model.sankey.SankeyData;
+import com.arms.dashboard.model.sankey.SankeyLink;
+import com.arms.dashboard.model.sankey.SankeyNode;
+import com.arms.product_service.pdservice.model.PdServiceEntity;
+import com.arms.product_service.pdservice.service.PdService;
+import com.arms.product_service.pdserviceversion.model.PdServiceVersionEntity;
+import com.arms.util.external_communicate.dto.search.검색결과;
 import com.arms.util.external_communicate.dto.search.검색결과_목록_메인;
 import com.arms.util.external_communicate.dto.지라이슈_제품_및_제품버전_검색요청;
 import com.arms.util.external_communicate.엔진통신기;
@@ -7,20 +15,89 @@ import com.arms.util.external_communicate.통계엔진통신기;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
 
     private final 엔진통신기 엔진통신기;
     private final 통계엔진통신기 통계엔진통신기;
+    private final PdService pdService;
 
+    @Override
     public 검색결과_목록_메인 commonNestedAggregation(final 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) {
         return 통계엔진통신기.제품_혹은_제품버전들의_집계_nested(지라이슈_제품_및_제품버전_검색요청).getBody();
     }
 
+    @Override
     public 검색결과_목록_메인 commonFlatAggregation(final 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) {
         return 통계엔진통신기.제품_혹은_제품버전들의_집계_flat(지라이슈_제품_및_제품버전_검색요청).getBody();
     }
 
+    @Override
+    public Map<String, RequirementJiraIssueAggregationResponse> requirementsJiraIssueStatuses(
+            final 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) {
+        return 통계엔진통신기.제품_혹은_제품버전들의_요구사항_지라이슈상태_월별_집계(지라이슈_제품_및_제품버전_검색요청).getBody();
+    }
+
+    @Override
+    public SankeyData sankeyChartAPI(final 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) throws Exception {
+        Long pdServiceLink = 지라이슈_제품_및_제품버전_검색요청.getPdServiceLink();
+        List<Long> pdServiceVersionLinks = 지라이슈_제품_및_제품버전_검색요청.getPdServiceVersionLinks();
+
+        if (지라이슈_제품_및_제품버전_검색요청.getPdServiceVersionLinks().isEmpty()) {
+            return new SankeyData(Collections.emptyList(), Collections.emptyList());
+        }
+
+        PdServiceEntity pdServiceEntity = new PdServiceEntity();
+        pdServiceEntity.setC_id(pdServiceLink);
+        PdServiceEntity savedPdService = pdService.getNode(pdServiceEntity);
+
+        List<SankeyNode> nodeList = new ArrayList<>();
+        List<SankeyLink> linkList = new ArrayList<>();
+
+        String pdServiceId = savedPdService.getC_id() + "-product";
+        nodeList.add(new SankeyNode(pdServiceId, savedPdService.getC_title(), "제품"));
+        nodeList.add(new SankeyNode("No-Worker", "No-Worker", "No-Worker"));
+
+        Set<Long> versionIds = savedPdService.getPdServiceVersionEntities().stream()
+                .filter(version -> pdServiceVersionLinks.contains(version.getC_id()))
+                .sorted(Comparator.comparing(PdServiceVersionEntity::getC_id)).map(version -> {
+                    String versionId = version.getC_id() + "-version";
+                    nodeList.add(new SankeyNode(versionId, version.getC_title(), "버전"));
+                    linkList.add(new SankeyLink(pdServiceId, versionId));
+                    return version.getC_id();
+                }).collect(Collectors.toSet());
+
+        Optional<List<검색결과>> optionalEsData =
+                Optional.ofNullable(통계엔진통신기.제품_혹은_제품버전들의_담당자목록(지라이슈_제품_및_제품버전_검색요청).getBody());
+        optionalEsData.ifPresent(esData -> {
+            esData.forEach(result -> {
+                String versionId = result.get필드명();
+                result.get하위검색결과().get("assignees").forEach(assignee -> {
+                    String assigneeAccountId = assignee.get필드명();
+                    assignee.get하위검색결과().get("displayNames").stream().forEach(displayName -> {
+                        String assigneeDisplayName = displayName.get필드명();
+                        String workerNodeId = versionId + "-" + assigneeAccountId;
+                        nodeList.add(new SankeyNode(workerNodeId, assigneeDisplayName, "작업자"));
+                        linkList.add(new SankeyLink(versionId + "-version", workerNodeId));
+                        versionIds.remove(Long.parseLong(versionId));
+                    });
+                });
+            });
+        });
+
+        versionIds.forEach(versionId -> linkList.add(new SankeyLink(versionId + "-version", "No-Worker")));
+
+        return new SankeyData(nodeList, linkList);
+    }
+
+    @Override
+    public List<Map<String, Object>> 작업자별_요구사항_관여도(final 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) throws IOException {
+        return 통계엔진통신기.작업자별_요구사항_관여도(지라이슈_제품_및_제품버전_검색요청).getBody();
+    }
 }
 
