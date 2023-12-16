33e1f9f9374a0356ea2f191e4e40ae5556820bc4
dumbbelloper <dumbbelloper@gmail.com>
2023-12-16 17:07:06 +0900

    feat: [ARMS-254] #comment Dashboard 코드 정리 - 23.12.16
    - Dashboard 일부 미사용 코드 제거
    - 비즈니스로직은 Service Layer 로 이전
    - 컨트롤러 return type 통일
    
    #close #time 1h +review SR @dumbbelloper
    
    See http://www.313.co.kr/jira/browse/ARMS-254 more details.
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/dashboard/service/DashboardService.java
+++ src/main/java/com/arms/dashboard/service/DashboardService.java
@@ -3,6 +3,7 @@ package com.arms.dashboard.service;
 import com.arms.dashboard.model.RequirementJiraIssueAggregationResponse;
 import com.arms.dashboard.model.SankeyData;
 import com.arms.util.external_communicate.dto.search.검색결과_목록_메인;
+import com.arms.util.external_communicate.dto.지라이슈_일반_검색_요청;
 import com.arms.util.external_communicate.dto.지라이슈_제품_및_제품버전_검색요청;
 
 import java.util.List;
@@ -18,4 +19,14 @@ public interface DashboardService {
     SankeyData sankeyChartAPI(지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) throws Exception;
 
     List<Map<String, Object>> 작업자별_요구사항_관여도(지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청);
+
+    Map<String, Long> 제품서비스별_담당자_이름_통계(Long pdServiceId);
+
+    검색결과_목록_메인 제품서비스_일반_통계(Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터);
+
+    Map<String, Object> 제품서비스_요구사항제외_일반_통계(Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터);
+
+    List<Object> 제품서비스_요구사항제외_일반_통계_TOP_5(Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터);
+
+    Map<String, Object> getIssueResponsibleStatusTop5(Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터);
 }
