07046139d9b8e52bbdcee1c0cd5c8101d01c5ac5
dumbbelloper <dumbbelloper@gmail.com>
2023-12-16 17:07:06 +0900

    feat: [ARMS-254] #comment Dashboard 코드 정리 - 23.12.16
    - Dashboard 일부 미사용 코드 제거
    - 비즈니스로직은 Service Layer 로 이전
    - 컨트롤러 return type 통일
    
    #close #time 1h +review SR @dumbbelloper
    
    See http://www.313.co.kr/jira/browse/ARMS-254 more details.
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/dashboard/controller/DashboardController.java
+++ src/main/java/com/arms/dashboard/controller/DashboardController.java
@@ -3,7 +3,6 @@ package com.arms.dashboard.controller;
 import com.arms.dashboard.model.RequirementJiraIssueAggregationResponse;
 import com.arms.dashboard.model.SankeyData;
 import com.arms.dashboard.service.DashboardService;
-import com.arms.util.external_communicate.dto.search.검색결과;
 import com.arms.util.external_communicate.dto.search.검색결과_목록_메인;
 import com.arms.util.external_communicate.dto.지라이슈_일반_검색_요청;
 import com.arms.util.external_communicate.dto.지라이슈_제품_및_제품버전_검색요청;
@@ -13,12 +12,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.*;
-
-import com.arms.util.external_communicate.*;
+import java.util.List;
+import java.util.Map;
 
 
 @Slf4j
@@ -26,10 +22,7 @@ import com.arms.util.external_communicate.*;
 @RequestMapping(value = "/arms/dashboard")
 @RequiredArgsConstructor
 public class DashboardController {
-    static final long DUMMY_JIRA_SERVER_ID = 0L;
 
-    private final 엔진통신기 엔진통신기;
-    private final 통계엔진통신기 통계엔진통신기;
     private final DashboardService dashboardService;
 
     @GetMapping("/aggregation/nested")
@@ -62,101 +55,48 @@ public class DashboardController {
      * Apache Echarts TreeMap Chart API - Dashboard, Analysis Resource
      */
     @GetMapping("/assignees-requirements-involvements")
-    ResponseEntity<ApiResult<List<Map<String, Object>>>> 작업자별_요구사항_관여도(
-            지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청
-    ) {
+    ResponseEntity<ApiResult<List<Map<String, Object>>>> 작업자별_요구사항_관여도(지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청) {
         return ResponseEntity.ok(CommonResponse.success(dashboardService.작업자별_요구사항_관여도(지라이슈_제품_및_제품버전_검색요청)));
     }
 
-    @GetMapping(value = "/getVersionProgress")
-    public ModelAndView getVersionProgress(HttpServletRequest request) {
-        /* 임시 틀 생성 */
-        String 제품서비스_아이디 = request.getParameter("pdserviceId");
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        modelAndView.addObject("result",
-                엔진통신기.제품서비스_버전별_상태값_통계(DUMMY_JIRA_SERVER_ID, 11L, 10L));
-
-        return modelAndView;
+    /**
+     * ReqStatus, Dashboard
+     */
+    @GetMapping("/jira-issue-assignee")
+    ResponseEntity<ApiResult<Map<String, Long>>> getJiraAssigneeList(@RequestParam Long pdServiceId) {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.제품서비스별_담당자_이름_통계(pdServiceId)));
     }
 
-    @GetMapping(value = "/assignee-jira-issue-statuses")
-    public ModelAndView getPerformancePerPerson(@RequestParam Long pdServiceLink) throws Exception {
-        Map<String, Map<String, Map<String, Integer>>> 통신결과 = 통계엔진통신기.담당자_요구사항여부_상태별집계(pdServiceLink);
-
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        modelAndView.addObject("result", 통신결과);
-
-        return modelAndView;
-    }
-
-
-    @GetMapping(value = "/jira-issue-assignee")
-    public ModelAndView getJiraAssigneeList(@RequestParam Long pdServiceId) {
-        Map<String, Long> 통신결과 = 통계엔진통신기.제품서비스별_담당자_이름_통계(pdServiceId);
-
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        modelAndView.addObject("result", 통신결과);
-
-        return modelAndView;
-    }
-
-
+    /**
+     * Dashboard
+     */
     @GetMapping("/normal/{pdServiceId}")
-    public ModelAndView normalAggregation(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
-        ResponseEntity<검색결과_목록_메인> 요구사항_연결이슈_일반_통계
-                = 통계엔진통신기.제품서비스_일반_통계(pdServiceId, 검색요청_데이터);
-
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        검색결과_목록_메인 통신결과 = 요구사항_연결이슈_일반_통계.getBody();
-        modelAndView.addObject("result", 통신결과);
-        return modelAndView;
+    ResponseEntity<ApiResult<검색결과_목록_메인>> normalAggregation(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.제품서비스_일반_통계(pdServiceId, 검색요청_데이터)));
     }
 
+    /**
+     * 현재 미사용
+     */
     @GetMapping("/exclusion-isreq-normal/{pdServiceId}")
-    public ModelAndView exclusionIsReqNormalAggregation(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
-        ResponseEntity<Map<String, Object>> 요구사항_연결이슈_일반_통계
-                = 통계엔진통신기.제품서비스_요구사항제외_일반_통계(pdServiceId, 검색요청_데이터);
-
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        Map<String, Object> 통신결과 = 요구사항_연결이슈_일반_통계.getBody();
-        modelAndView.addObject("result", 통신결과);
-        return modelAndView;
+    ResponseEntity<ApiResult<Map<String, Object>>> exclusionIsReqNormalAggregation(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.제품서비스_요구사항제외_일반_통계(pdServiceId, 검색요청_데이터)));
     }
 
+    /**
+     * Dashboard
+     */
     @GetMapping("/exclusion-isreq-normal/req-and-linked-issue-top5/{pdServiceId}")
-    public ModelAndView getReqAndLinkedIssueTop5(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
-        ResponseEntity<Map<String, Object>> 요구사항_연결이슈_일반_통계
-                = 통계엔진통신기.제품서비스_요구사항제외_일반_통계(pdServiceId, 검색요청_데이터);
-
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        Map<String, Object> 통신결과 = 요구사항_연결이슈_일반_통계.getBody();
-
-        Map<String, Object> 검색결과 = (Map<String, Object>) 통신결과.get("검색결과");
-        List<Object> 작업자별결과 = (List<Object>) 검색결과.get("group_by_assignee.assignee_emailAddress.keyword");
-        modelAndView.addObject("result", 작업자별결과);
-        return modelAndView;
+    ResponseEntity<ApiResult<List<Object>>> getReqAndLinkedIssueTop5(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.제품서비스_요구사항제외_일반_통계_TOP_5(pdServiceId, 검색요청_데이터)));
     }
 
+    /**
+     * Dashboard
+     */
     @GetMapping("/normal/issue-responsible-status-top5/{pdServiceId}")
-    public ModelAndView getIssueResponsibleStatusTop5(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
-        ResponseEntity<검색결과_목록_메인> 요구사항_연결이슈_일반_통계
-                = 통계엔진통신기.제품서비스_일반_통계(pdServiceId, 검색요청_데이터);
-
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        검색결과_목록_메인 검색결과목록 = 요구사항_연결이슈_일반_통계.getBody();
-        List<검색결과> 작업자별결과 = 검색결과목록.get검색결과().get("group_by_assignee.assignee_emailAddress.keyword");
-
-        Map<String, Object> personAndStatus = new HashMap<>();
-        for (검색결과 obj : 작업자별결과) {
-            String 작업자메일 = obj.get필드명();
-            int 엣위치 = 작업자메일.indexOf("@");
-            String 작업자아이디 = 작업자메일.substring(0, 엣위치);
-            Map<String, List<검색결과>> 하위검색_이슈상태 = (Map<String, List<검색결과>>) obj.get하위검색결과();//("group_by_assignee.assignee_emailAddress.keyword");
-            personAndStatus.put(작업자아이디, 하위검색_이슈상태);
-        }
-
-        modelAndView.addObject("result", personAndStatus);
-        return modelAndView;
+    ResponseEntity<ApiResult<Map<String, Object>>> getIssueResponsibleStatusTop5(@PathVariable("pdServiceId") Long pdServiceId, 지라이슈_일반_검색_요청 검색요청_데이터) {
+        return ResponseEntity.ok(CommonResponse.success(dashboardService.getIssueResponsibleStatusTop5(pdServiceId, 검색요청_데이터)));
     }
 
 }
