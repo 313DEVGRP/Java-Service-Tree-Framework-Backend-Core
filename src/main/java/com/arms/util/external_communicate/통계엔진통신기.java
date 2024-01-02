6c35cd1c77a2080ff21932c96d7601c6256e72aa
yhs <gudtjr4662@gmail.com>
2024-01-03 00:52:44 +0900

    feat: [ARMS-242] #comment [스코프 분석] 요구사항_별_상태_및_관여_작업자_수 - 호출 방식 변경 - 24.01.02 #close #time 1h +review SR @HSYang-Hub
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/util/external_communicate/통계엔진통신기.java
+++ src/main/java/com/arms/util/external_communicate/통계엔진통신기.java
@@ -107,10 +107,7 @@ public interface 통계엔진통신기 {
     );
 
     @GetMapping("/engine/jira/dashboard/req-status-and-reqInvolved-unique-assignees")
-    ResponseEntity<List<제품_서비스_버전>> 요구사항_별_상태_및_관여_작업자_수(@SpringQueryMap 지라이슈_제품_및_제품버전_병합_검색_요청 지라이슈_제품_및_제품버전_검색요청);
-
-    @GetMapping("/engine/jira/dashboard/req-status-and-reqInvolved-unique-assignees2")
-    ResponseEntity<List<제품_서비스_버전>> 요구사항_별_상태_및_관여_작업자_수3(@SpringQueryMap 지라이슈_제품_및_제품버전_검색요청 지라이슈_제품_및_제품버전_검색요청);
+    ResponseEntity<List<제품_서비스_버전>> 요구사항_별_상태_및_관여_작업자_수(@SpringQueryMap 지라이슈_제품_및_제품버전_병합_집계_요청 지라이슈_제품_및_제품버전_검색요청);
 
     @GetMapping("/engine/jira/dashboard/requirement-linkedissue/{pdServiceId}")
     ResponseEntity<List<지라이슈>> 제품별_요구사항_연결이슈_조회(
