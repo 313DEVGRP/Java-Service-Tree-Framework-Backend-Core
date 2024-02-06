2ad34555cb0ad24d5122b897eddb1442aa1979a5
Advanc2d <phm940916@gmail.com>
2024-02-06 23:05:33 +0900

    :see_no_evil: refactor: [PHM-87] #comment 버전_요구사항_담당자 API, 요구사항 조회 API 데이터 타입 수정 - 24.02.06
    key value(key value(key value) 형태로 담당자 데이터를 찾아갈 수 있도록 하기 위함
    
    #close #time 1h +review SR @313devops
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::Advanc2d ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/analysis/cost/dto/버전요구사항별_담당자데이터.java
+++ src/main/java/com/arms/api/analysis/cost/dto/버전요구사항별_담당자데이터.java
@@ -7,20 +7,8 @@ import java.util.Map;
 @Setter
 @Getter
 public class 버전요구사항별_담당자데이터 {
-    private Map<String, 버전데이터> 버전;
+    private Map<String, Map<String, Map<String, 담당자데이터>>> 버전_요구사항_담당자;
     private Map<String, 담당자데이터> 전체담당자목록;
-
-    @Getter
-    @Builder
-    public static class 버전데이터 {
-        private Map<String,요구사항데이터> 요구사항;
-    }
-
-    @Getter
-    @Builder
-    public static class 요구사항데이터 {
-        private Map<String,담당자데이터> 담당자;
-    }
 
     @Getter
     @Builder
