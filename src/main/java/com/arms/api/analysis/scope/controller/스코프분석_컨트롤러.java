01e4d1bd6c146c529a5470a5a0437eafa6255db4
hsYang <gudtjr4662@gmail.com>
2024-02-26 01:35:30 +0900

    feat: [YHS-67] #comment 사용하지않는 API 정리(스코프분석 일부) -24.02.25 #close #time 1h +review SR @HSYang-Hub
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::hsYang ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/analysis/scope/controller/스코프분석_컨트롤러.java
+++ src/main/java/com/arms/api/analysis/scope/controller/스코프분석_컨트롤러.java
@@ -75,44 +75,6 @@ public class 스코프분석_컨트롤러 {
         return  ResponseEntity.ok(버전_요구사항_수);
     }
 
-    @PostMapping("/req-status-and-reqInvolved-unique-assignees")
-    public ResponseEntity<List<제품_서비스_버전>> 요구사항_별_상태_및_관여_작업자_수(
-           @RequestBody 지라이슈_제품_및_제품버전_병합_집계_요청 지라이슈_제품_및_제품버전_병합_집계_요청) {
-        Long 선택된_제품서비스_아이디 = 지라이슈_제품_및_제품버전_병합_집계_요청.get요구_사항().getPdServiceLink();
-        List<Long> 선택된_버전_목록 = 지라이슈_제품_및_제품버전_병합_집계_요청.get요구_사항().getPdServiceVersionLinks();
-        log.info("[ 스코프분석_컨트롤러 :: 요구사항_별_상태_및_관여_작업자_수 ] 백엔드 요청");
-        log.info("요구_사항 :: 선택된_제품_서비스 아이디 : {}", 선택된_제품서비스_아이디);
-        log.info("요구_사항 :: 선택된_제품_서비스 버전 : {}", 선택된_버전_목록);
-
-        ResponseEntity<List<제품_서비스_버전>> 요구사항_별_상태_및_관여_작업자_수_통신결과 = 통계엔진통신기.요구사항_별_상태_및_관여_작업자_수(지라이슈_제품_및_제품버전_병합_집계_요청);
-
-        String 하위그룹필드 = "key,status.status_name.keyword"; // yml or static 으로
-        지라이슈_일반_집계_요청 일반_집계_요청_세팅 = 지라이슈_일반_집계_요청.builder()
-                .isReq(true)
-                .메인그룹필드("pdServiceVersions")
-                .컨텐츠보기여부(true)
-                .크기(1000)
-                .하위그룹필드들(Arrays.stream(하위그룹필드.split(",")).collect(Collectors.toList()))
-                .build();
-
-        ResponseEntity<검색결과_목록_메인> 제품서비스_일반_버전_통계_통신결과 =
-                통계엔진통신기.제품서비스_일반_버전_통계(선택된_제품서비스_아이디, 선택된_버전_목록, 일반_집계_요청_세팅);
-
-        List<제품_서비스_버전> 매핑결과 = Optional.ofNullable(요구사항_별_상태_및_관여_작업자_수_통신결과)
-                .map(ResponseEntity::getBody)
-                .flatMap(요구사항_별_상태_및_관여_작업자_수 ->
-                        Optional.ofNullable(제품서비스_일반_버전_통계_통신결과)
-                                .map(ResponseEntity::getBody)
-                                .map(검색결과_목록_메인::get검색결과)
-                                .map(검색결과 -> scopeService.요구사항_상태_매핑(요구사항_별_상태_및_관여_작업자_수, 검색결과))
-                )
-                .orElseGet(() -> {
-                    log.info("[ 스코프분석_컨트롤러 :: 요구사항_별_상태_및_관여_작업자_수 ] 매핑결과가 null 입니다.");
-                    return null;
-                });
-        return ResponseEntity.ok(매핑결과);
-    }
-
     @GetMapping("/{pdServiceId}/req-status-and-reqInvolved-unique-assignees-per-version")
     public ResponseEntity<List<버전별_요구사항_상태_작업자수>> 버전배열_요구사항_별_상태_및_관여_작업자_수(@PathVariable("pdServiceId") Long pdServiceId,
                                                                             @RequestParam List<Long> pdServiceVersionLinks) throws Exception {
