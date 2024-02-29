ad2ee1ac2d63b98eef22de621cc64a4803881a88
dumbbelloper <dumbbelloper@gmail.com>
2024-02-29 20:28:54 +0900

    ⚡️refactor: [DEMO-38] #comment Slack 알림 리팩토링 - 24.02.29
    #close #time 1m +review SR @dumbbelloper
    
    See https://dumbbelloper.atlassian.net/browse/DEMO-38 more details.
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/notification/slack/SlackConfig.java
+++ src/main/java/com/arms/notification/slack/SlackConfig.java
@@ -3,17 +3,14 @@ package com.arms.notification.slack;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @EnableConfigurationProperties(value = SlackProperty.class)
 public class SlackConfig {
-    @Bean
-    public SlackApiService slackService(SlackProperty slackProperty) {
-        return new SlackApiService(slackProperty);
-    }
 
     @Bean
-    public SlackService slackClient(SlackApiService slackApiService) {
-        return new SlackService(slackApiService);
+    public SlackNotificationService slackNotificationService(SlackProperty slackProperty, Environment environment) {
+        return new SlackNotificationService(slackProperty, environment);
     }
 }
