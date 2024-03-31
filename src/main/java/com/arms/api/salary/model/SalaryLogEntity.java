35c5ad2af0835c5a2bf1359d16df84a46c68ab19
dumbbelloper <dumbbelloper@gmail.com>
2024-03-31 16:21:33 +0900

    ⚡️feat: [DEMO-136] #comment SalaryLog 추가 - 24.03.31
    #close #time 1m +review SR @dumbbelloper
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/arms/api/salary/model/SalaryLogEntity.java
@@ -0,0 +1,100 @@
+/*
+ * @author Dongmin.lee
+ * @since 2023-03-21
+ * @version 23.03.21
+ * @see <pre>
+ *  Copyright (C) 2007 by 313 DEV GRP, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by 313 developer group <313@313.co.kr>, December 2010
+ * </pre>
+ */
+package com.arms.api.salary.model;
+
+import com.arms.egovframework.javaservice.treeframework.model.TreeBaseEntity;
+import com.arms.egovframework.javaservice.treeframework.model.TreeLogBaseEntity;
+import com.arms.egovframework.javaservice.treeframework.model.TreeSearchEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SelectBeforeUpdate;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "T_ARMS_ANNUAL_INCOME_LOG")
+@SelectBeforeUpdate(value = true)
+@DynamicInsert(value = true)
+@DynamicUpdate(value = true)
+@Cache(usage = CacheConcurrencyStrategy.NONE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class SalaryLogEntity extends TreeLogBaseEntity implements Serializable {
+
+
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "c_id")
+    public Long getC_id() {
+        return super.getC_id();
+    }
+
+    @Column(name = "c_name")
+    private String c_name;
+
+    @Column(name = "c_key")
+    private String c_key;
+
+    @Column(name = "c_annual_income")
+    private String c_annual_income;
+    /*
+     * Extend Bean Field
+     */
+    @JsonIgnore
+    private Boolean copyBooleanValue;
+
+    @Transient
+    @ApiModelProperty(hidden = true)
+    public Boolean getCopyBooleanValue() {
+        copyBooleanValue = false;
+        if (this.getCopy() == 0) {
+            copyBooleanValue = false;
+        } else {
+            copyBooleanValue = true;
+        }
+        return copyBooleanValue;
+    }
+
+    public void setCopyBooleanValue(Boolean copyBooleanValue) {
+        this.copyBooleanValue = copyBooleanValue;
+    }
+
+    @Override
+    public <T extends TreeSearchEntity> void setFieldFromNewInstance(T paramInstance) {
+        if (paramInstance instanceof TreeBaseEntity) {
+            if (paramInstance.isCopied()) {
+                this.setC_title("copy_" + this.getC_title());
+            }
+        }
+    }
+}
