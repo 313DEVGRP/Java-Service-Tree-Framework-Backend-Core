30415da16c8dba690f938eecbb409f8ea5cf6170
dumbbelloper <dumbbelloper@gmail.com>
2024-04-04 10:58:25 +0900

    fix: [ARMSQA-19] #comment OpenFeign 내부통신기 오류 해결 - 24.04.04
    관련 커밋 : https://github.com/313DEVGRP/Java-Service-Tree-Framework-Backend-Core/commit/f2bc19ecae95ecfcd99697cb0feb12ecf1981f20
    
    #close #time 1h +review SR @313devops
    
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio

--- src/main/java/com/arms/api/util/communicate/internal/내부통신기.java
+++ src/main/java/com/arms/api/util/communicate/internal/내부통신기.java
@@ -4,6 +4,7 @@ import com.arms.api.requirement.reqadd.model.LoadReqAddDTO;
 import com.arms.api.requirement.reqstatus.model.ReqStatusDTO;
 import com.arms.api.requirement.reqstatus.model.ReqStatusEntity;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,20 +14,19 @@ import java.util.List;
 public interface 내부통신기 {
 
     @PostMapping("/arms/reqStatus/{changeReqTableName}/addStatusNode.do")
-    ResponseEntity<?> 요구사항_이슈_저장하기(@PathVariable(value ="changeReqTableName") String changeReqTableName,
-                                   @RequestBody ReqStatusDTO reqStatusDTO);
+    ResponseEntity<?> 요구사항_이슈_저장하기(@PathVariable(value = "changeReqTableName") String changeReqTableName,
+            @RequestBody ReqStatusDTO reqStatusDTO);
 
     @GetMapping("/arms/reqStatus/{changeReqTableName}/getStatusMonitor.do")
-    List<ReqStatusEntity> 제품별_요구사항_이슈_조회(@PathVariable(value ="changeReqTableName") String changeReqTableName,
-                                         @RequestBody ReqStatusDTO reqStatusDTO);
+    List<ReqStatusEntity> 제품별_요구사항_이슈_조회(@PathVariable(value = "changeReqTableName") String changeReqTableName,
+            @SpringQueryMap ReqStatusDTO reqStatusDTO);
 
     @PutMapping("/arms/reqStatus/{changeReqTableName}/updateStatusNode.do")
-    ResponseEntity<?> 요구사항_이슈_수정하기(@PathVariable(value ="changeReqTableName") String changeReqTableName, @RequestBody ReqStatusDTO reqStatusDTO);
+    ResponseEntity<?> 요구사항_이슈_수정하기(@PathVariable(value = "changeReqTableName") String changeReqTableName,
+            @RequestBody ReqStatusDTO reqStatusDTO);
 
     @GetMapping("/arms/reqAdd/{changeReqTableName}/getNode.do/{c_id}")
-    ResponseEntity<LoadReqAddDTO> 요구사항조회(
-            @PathVariable(value = "changeReqTableName") String changeReqTableName,
-            @PathVariable(value = "c_id") Long c_id
-    );
+    ResponseEntity<LoadReqAddDTO> 요구사항조회(@PathVariable(value = "changeReqTableName") String changeReqTableName,
+            @PathVariable(value = "c_id") Long c_id);
 
 }
