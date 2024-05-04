b99106074a15d252345ca0c402ea481c1bb8d758
dumbbelloper <dumbbelloper@gmail.com>
2024-05-04 23:56:43 +0900

    feat: [DEMO-210] #comment 기존에 등록한 제품 관련 파일들 글로벌 컨텐츠 트리 맵으로 마이그레이션 - 24.05.04
    #close #time 1h +review SR @313devops
    
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio

--- src/main/java/com/arms/api/migration/MigrationController.java
+++ src/main/java/com/arms/api/migration/MigrationController.java
@@ -1,17 +1,27 @@
 package com.arms.api.migration;
 
+import com.arms.api.globaltreemap.model.GlobalContentsTreeMapEntity;
+import com.arms.api.globaltreemap.model.GlobalTreeMapEntity;
+import com.arms.api.globaltreemap.service.GlobalContentsTreeMapService;
+import com.arms.api.globaltreemap.service.GlobalTreeMapService;
 import com.arms.api.jira.jiraserver.model.JiraServerEntity;
 import com.arms.api.jira.jiraserver.service.JiraServer;
 import com.arms.api.product_service.pdservice.model.PdServiceEntity;
 import com.arms.api.product_service.pdservice.service.PdService;
+import com.arms.api.product_service.pdservice_detail.model.PdServiceDetailEntity;
+import com.arms.api.product_service.pdservice_detail.service.PdServiceDetail;
+import com.arms.api.product_service.pdservice_pure.model.PdServicePureEntity;
+import com.arms.api.product_service.pdservice_pure.service.PdServicePure;
 import com.arms.api.requirement.reqstatus.model.ReqStatusDTO;
 import com.arms.api.requirement.reqstatus.model.ReqStatusEntity;
 import com.arms.api.util.communicate.external.엔진통신기;
 import com.arms.api.util.communicate.internal.내부통신기;
+import com.arms.egovframework.javaservice.treeframework.TreeConstant;
 import com.arms.egovframework.javaservice.treeframework.controller.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +30,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/arms/migration")
@@ -28,9 +39,13 @@ import java.util.function.Function;
 public class MigrationController {
 
     private final PdService pdService;
+    private final PdServicePure pdServicePure;
     private final JiraServer jiraServer;
+    private final PdServiceDetail pdServiceDetail;
     private final 내부통신기 internalCommunicator;
     private final 엔진통신기 externalCommunicator;
+    private final GlobalTreeMapService globalTreeMapService;
+    private final GlobalContentsTreeMapService globalContentsTreeMapService;
 
     @GetMapping("/update-req-link")
     public ResponseEntity<CommonResponse.ApiResult<String>> updateReqLink() throws Exception {
@@ -83,4 +98,41 @@ public class MigrationController {
         return ResponseEntity.ok(CommonResponse.success("ok"));
     }
 
+
+    @GetMapping("/pdservice-detail")
+    @Transactional
+    public ResponseEntity<CommonResponse.ApiResult<String>> updatePdServiceDetail() throws Exception {
+        List<PdServicePureEntity> nodesWithoutRoot = pdServicePure.getNodesWithoutRoot(new PdServicePureEntity());
+        for (PdServicePureEntity pdServicePureEntity : nodesWithoutRoot) {
+            // 1. PdService 조회하여 id, contents 추출
+            String pdServiceContents = pdServicePureEntity.getC_pdservice_contents();
+            Long pdServiceId = pdServicePureEntity.getC_id();
+
+            // 2. PdService id, contents 활용하여 BaseDetail 추가
+            PdServiceDetailEntity pdServiceDetailEntity = new PdServiceDetailEntity();
+            pdServiceDetailEntity.setRef(TreeConstant.First_Node_CID);
+            pdServiceDetailEntity.setC_title("BaseDetail");
+            pdServiceDetailEntity.setC_type("default");
+            pdServiceDetailEntity.setC_contents(pdServiceContents);
+            PdServiceDetailEntity detailEntity = pdServiceDetail.addNodeWithGlobalContentsTreeMap(pdServiceId, pdServiceDetailEntity);
+
+            Long detailId = detailEntity.getC_id();
+
+            // 3. 글로벌 트리맵 조회하여 제품에 등록한 파일 목록 가져오기
+            List<GlobalTreeMapEntity> globalTreeMapEntities = globalTreeMapService.findAllByIds(List.of(pdServiceId), "pdservice_link");
+            List<GlobalTreeMapEntity> filteredGlobalTreeMapEntities = globalTreeMapEntities.stream().filter(tm -> tm.getFilerepository_link() != null).collect(Collectors.toList());
+
+            // 4. 기존 제품에 등록 된 파일 목록 등을 글로벌 컨텐츠 트리 맵에 등록
+            for (GlobalTreeMapEntity globalTreeMapEntity : filteredGlobalTreeMapEntities) {
+                Long fileId = globalTreeMapEntity.getFilerepository_link();
+                GlobalContentsTreeMapEntity addGlobalContentsTreeMapEntity = new GlobalContentsTreeMapEntity();
+                addGlobalContentsTreeMapEntity.setPdservicedetail_link(detailId);
+                addGlobalContentsTreeMapEntity.setFilerepository_link(fileId);
+                globalContentsTreeMapService.saveOne(addGlobalContentsTreeMapEntity);
+            }
+
+        }
+
+        return ResponseEntity.ok(CommonResponse.success("ok"));
+    }
 }
